8025442a75f1a0fa217600c6db8e018c0469eaf6
DESKTOP-EIONEP7\Agustin <agusdel_94@hotmail.com>
2016-10-24 23:28:50 -0300

    ++ TP terminado.

--- New Unity Project/Assets/Script/Points.cs
+++ New Unity Project/Assets/Script/Points.cs
@@ -1,16 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class Points : MonoBehaviour {
 
-	
 	public TextMesh puntosText;
-    public float puntos = 0;
+    private float puntos = 0;
 
-    public void Update()
+
+    public void Start()
+    {
+        puntos = 0;
+        puntosText.text = Convert.ToString(puntos);
+    }
+
+    /*public void Update()
     {
         puntos = puntos + 1;
         puntosText.text = "" + puntos;
+    }*/
+
+    public void AumentarContador()
+    {
+        puntos++;
+        puntosText.text = Convert.ToString(puntos);
     }
 	
 	
