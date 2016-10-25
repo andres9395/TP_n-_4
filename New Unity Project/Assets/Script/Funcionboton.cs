5a593c3f2ae75ee5e184ae1a20f1ec4d5c7aa9b5
Neko-PC\Neko <retepio@hotmail.com>
2016-10-24 22:20:37 -0300

    5

---
+++ New Unity Project/Assets/Script/Funcionboton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*public class funcionBOTON : MonoBehaviour {
+
+    public GameObject puntos;
+
+    public void SpawnPuntos()
+    {
+        Vector3 puntosmas = new Vector3
+    }
+}
---
+++ New Unity Project/Assets/Script/Points.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Points : MonoBehaviour {
+
+	
+	public TextMesh puntosText;
+    public float puntos = 0;
+
+    public void Update()
+    {
+        puntos = puntos + 1;
+        puntosText.text = "" + puntos;
+    }
+	
+	
+}
